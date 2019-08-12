4f45b16f3b281da3f33b0c9b56f60fc861ea2305
raowr <891770552@qq.com>
2019-08-12 22:57:59 +0800

    我的相册列表优化

--- controllers/home/share.go
+++ controllers/home/share.go
@@ -9,21 +9,36 @@ type ShareController struct {
 	BaseController
 }
 
-func (c *ShareController) Index()  {
+func (c *ShareController) Index() {
 	album := new(models.Album)
 	var albumList []*models.Album
-	if c.Ctx.Request.Method == "POST"{
-		page,_ := c.GetInt("page")
-		album.Query().Filter("Status",2).Limit(8,(page-1)*8).All(&albumList)
-		c.Data["json"] = albumList
+	if c.Ctx.Request.Method == "POST" {
+		page, _ := c.GetInt("page")
+		album.Query().Filter("Status", 2).Limit(8, (page-1)*8).All(&albumList)
+		albumlists := c.getAlbumlists(albumList)
+		c.Data["json"] = albumlists
 		c.ServeJSON()
-	}else {
-		album.Query().Filter("Status",2).Limit(8,0).All(&albumList)
-		albumCount,_ := album.Query().Filter("Status",2).Count()
-		c.Data["albumList"] = albumList
-		c.Data["pageTotal"] = math.Ceil(float64(albumCount)/8)
+	} else {
+		album.Query().Filter("Status", 2).Limit(8, 0).All(&albumList)
+		albumlists := c.getAlbumlists(albumList)
+		albumCount, _ := album.Query().Filter("Status", 2).Count()
+		c.Data["albumList"] = albumlists
+		c.Data["pageTotal"] = math.Ceil(float64(albumCount) / 8)
 		c.Data["dataTotal"] = albumCount
 		c.display("home/share")
 	}
+}
 
+func (c *ShareController) getAlbumlists(albumList []*models.Album) (albumlists []interface{}) {
+	for i := 0; i < 4; i++ {
+		albums := make(map[int]interface{})
+		if i < len(albumList) {
+			albums[0] = albumList[i]
+		}
+		if i+4 < len(albumList) {
+			albums[1] = albumList[i+4]
+		}
+		albumlists = append(albumlists, albums)
+	}
+	return
 }
